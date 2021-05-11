42c52c936b6815924ce5630fa895677811f8e880
Ygor Nogatz <ynogatzds@gmail.com>
2021-05-11 14:12:07 -0300

    adicionado Procfile e rota de teste

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const routes = express.Router();
 
-
+routes.get('/', (req, res) => {
+    res.send(`
+        <h1>Página inicial da aplicação</h1>
+        <h3>Rota de teste</h3>
+    `)
+})
 
 module.exports = routes;
