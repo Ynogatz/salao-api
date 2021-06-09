85f9bca1c470030367091ae8fb8933d8d8eff97e
Ygor Nogatz <ynogatzds@gmail.com>
2021-06-09 15:51:53 -0300

    Mudanças no usuario-controller

--- src/usuario/usuario-controller.js
+++ src/usuario/usuario-controller.js
@@ -2,19 +2,20 @@ const Usuario = require('./usuario')
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken');
 const authConfig = require('../config/auth')
+const UsuarioService = require('./usuario-service');
+const usuarioService = new UsuarioService;
 
 function generateToken(params) {
     return jwt.sign({ usuario: params }, authConfig.secret, {
         expiresIn: 86400
     })
 }
-
 class UsuarioController {
     async index(req, res, next) {
         try {
             const usuarios = await Usuario.find();
             res.send(`
-            <h1>Página inicial da aplicação</h1>
+            <h1>Página da aplicação</h1>
             <h3>Lista de usuários</h3>
             ${usuarios}
         `)
@@ -40,24 +41,36 @@ class UsuarioController {
         try {
             const { login, senha } = req.body;
 
-            const usuario = await Usuario.findOne({ login }).select('+senha')
+            if (usuarioService.verificaCampos(login, senha)) {
+                const usuario = await Usuario.findOne({ login }).select('+senha')
 
-            if (!usuario) return res.status(400).send({ erro: "Usuário não encontrado" })
+                if (!usuario) return res.status(400).send({ erro: "Usuário não encontrado" })
 
-            if (!await bcrypt.compare(senha, usuario.senha)) return res.status(400).send({ erro: "Senha inválida" })
+                if (!await bcrypt.compare(senha, usuario.senha)) return res.status(400).send({ erro: "Senha inválida" })
 
-            usuario.senha = undefined
+                usuario.senha = undefined
 
-            res.send({
-                usuario,
-                token:generateToken(usuario)
-            }).status(200)
+
+                res.send({
+                    token: generateToken(usuario)
+                }).status(200)
+            } else res.send({ erro: 'Login e senha devem ser preenchidos' }).status(400)
+
 
         } catch (erro) {
             next({ erro })
         }
     }
 
+    teste(req, res, next) {
+        let token = req.headers.authorization
+
+        token = token.split(' ')[1]
+
+        const userData = jwt.decode(token)
+
+        res.send(userData.usuario.senha)
+    }
 
 }
 module.exports = UsuarioController;
